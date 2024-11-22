9d88e020e7d4904e8e2bc71599f148bef4217755
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-22 12:45:07 +0530

    favicon and title changed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import MainPage from './components/main/mainPage.jsx';
 function App() {
 
+  document.title="TaskHive"
   return (
     <div className="App">
       {/* {console.log(`env variable is `,process.env.REACT_APP_PORT)} */}
--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -41,7 +41,7 @@ function Search() {
         onChange={(e) => setSearchQuery(e.target.value)}
         className='searchBox h-16 w-3/5 px-8 mx-12 rounded-full'
       />
-      <ul className='px-16 my-12 text-2xl'>
+      <ul className='px-16 my-12 text-xl'>
         {filteredTasks.map((task) => (
           <li key={task.id} className='mb-3 px-8 w-3/4 py-5 bg-white list-disc '>
             <span>{task.description}</span>
